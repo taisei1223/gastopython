082c515744556488af0d4f2355e843b634ff00b0
taisei1223 <taisei12232000m@gmail.com>
2024-09-23 20:25:32 +0900

    'tests]'

--- save_cookies.py
+++ save_cookies.py
@@ -1,24 +1,35 @@
+#%%
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 import pickle
 import time
 
 # Chromeオプションを設定
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')  # ヘッドレスモードで実行
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--remote-debugging-port=9222')  # 追加のデバッグポート
+cService = webdriver.ChromeService(executable_path=ChromeDriverManager().install())
+options = Options()
+#options.add_argument('--no-sandbox')
+#options.add_argument('--headless')
+
+
+
+
+#options = webdriver.ChromeOptions()
+#options.add_argument('--headless')  # ヘッドレスモードで実行
+#options.add_argument('--no-sandbox')
+#options.add_argument('--disable-dev-shm-usage')
 
 # Chromeドライバーのパスを指定
-driver = webdriver.Chrome(options=options)
+#driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(service=cService, options=options)
+
 
 # MyFitnessPalのログインページにアクセス
 driver.get('https://www.myfitnesspal.com/account/login')
+print(driver.page_source)
 
-# ユーザー名とパスワードを入力
-time.sleep(3)
 #driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[2]/div/div[4]/button[1]").click()
 username = driver.find_element(By.XPATH, '//*[@id="email"]')
 password = driver.find_element(By.XPATH, '//*[@id="password"]')
@@ -33,3 +44,4 @@ with open('cookies.pkl', 'wb') as f:
 
 # ブラウザを閉じる
 driver.quit()
+#%%
